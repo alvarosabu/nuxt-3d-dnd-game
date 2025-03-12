41158911815bca92eef2ad85e7ddfb7106596ba2
alvarosabu <alvaro.saburido@gmail.com>
2025-03-12 18:13:45 +0100

    feat: level

--- components/environments/World.vue
+++ components/environments/World.vue
@@ -1,22 +1,19 @@
 <script setup lang="ts">
-
 const gameStore = useGameStore()
 const { getResource } = useResourcePreloader()
-// const { scene, animations } = getResource('models', gameStore.currentLevel.value.model)
-
-console.dir(gameStore.currentLevel)
+const { scene } = getResource('models', gameStore.currentLevel.model)
 
 const modelRef = ref()
 </script>
 
 <template>
-    <primitive
-      v-if="scene"
-      ref="modelRef"
-      :name="scene"
-      :object="scene"
-    />
-    <!-- <TresMesh
+  <primitive
+    v-if="scene"
+    ref="modelRef"
+    :name="scene"
+    :object="scene"
+  />
+  <!-- <TresMesh
       :rotation-x="-Math.PI / 2"
       class="cursor-pointer"
     >
